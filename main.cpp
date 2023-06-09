4d491fbbfdc36a3c23e30eb54c07525a1cb2a657
prof_redstone <tom.demagny@laposte.net>
2023-06-09 21:07:57 +0200

    argument pass ok

--- main.cpp
+++ main.cpp
@@ -22,25 +22,14 @@ double bouleSize = 30; //rayon de la taille des boules en pixel dans la simulati
 int nbBrasseur = 40; 
 double brasseurSize = 12; //rayon de la taille des brasseurs en pixel dans la simulation 
 double brasseurSpeed = 0.7; //vitesse de rotation des brasseurs en rad/frame
-int seed = 4; //seed pour la position des balles
+int seed = 6; //seed pour la position des balles
 int nbTirage = 5;
 double timebtwTirage = 500; //unite random
 
+void getParam(int argc, char* argv[]);//y'a pas de .h
+
 void main(int argc, char* argv[]){
-    cout << "Nombre de parametre : " + to_string(argc) << endl;
-    for (int i = 0; i < argc; i++){
-        cout << "param " + to_string(i) + " : " << argv[i] << endl;
-    }
-    cout << "Delta Time : " + to_string(dt) << endl; //devra etre fixe
-    cout << "Nombre de boules : " + to_string(nbBoule) << endl;
-    cout << "Taille boules : " + to_string(bouleSize) << endl; //devra etre fixe
-    cout << "Seed for noise : " + to_string(seed) << endl;
-    cout << "Noise factor : " + to_string(noise) << endl;
-    cout << "Nombre brasseur : " + to_string(nbBrasseur) << endl; //devra etre fixe
-    cout << "Brasseur speed : " + to_string(brasseurSpeed) << endl; //devra etre fixe
-    cout << "Brasseur size : " + to_string(brasseurSize) << endl; //devra etre fixe
-    cout << "Square size : " + to_string(WIN_WIDTH) << endl; //devra etre fixe
-    cout << "Nombre frame skip :" + to_string(nbFrameSkip) << endl; //devra etre fixe
+    getParam(argc, argv);
 
     RenderWindow window(VideoMode(WIN_WIDTH, WIN_HEIGHT, 32), "TIPE SIMULATION");
     window.setTitle("Simulation TIPE");
@@ -81,4 +70,58 @@ void main(int argc, char* argv[]){
     cout << endl;
 
     return ;
+}
+
+void getParam(int argc, char* argv[]) {
+
+    //cout << "Nombre de parametre : " + to_string(argc) << endl;
+
+
+    for (int i = 1; i < argc-1; i++) {//-1 pour faire +1 
+        //cout << "param " + to_string(i) + " : " << argv[i] << endl;
+        if (strcmp(argv[i], "seed") == 0) {
+             seed = stoi(argv[i + 1]); 
+        }
+        if (strcmp(argv[i], "nbtirage") == 0) {
+            nbTirage = stoi(argv[i + 1]);
+        }
+        if (strcmp(argv[i], "nbboule") == 0) {
+            nbBoule = stoi(argv[i + 1]);
+        }
+        if (strcmp(argv[i], "sizeboule") == 0) {
+            bouleSize = stod(argv[i + 1]);
+        }
+        if (strcmp(argv[i], "noise") == 0) {
+            noise = stod(argv[i + 1]);
+        }
+        if (strcmp(argv[i], "brasseurSpeed") == 0) {
+            brasseurSpeed = stod(argv[i + 1]);
+        }
+        if (strcmp(argv[i], "inttirage") == 0) {
+            timebtwTirage = stod(argv[i + 1]);
+        }
+        if (strcmp(argv[i], "nbFrameSkip") == 0) {
+            nbFrameSkip = stoi(argv[i + 1]);
+        }
+        if (strcmp(argv[i], "dt") == 0) {
+            dt = stod(argv[i + 1]);
+        }
+        if (strcmp(argv[i], "size") == 0) {
+            WIN_WIDTH = stoi(argv[i + 1]);
+            WIN_HEIGHT = stoi(argv[i + 1]);
+        }
+    }
+
+    cout << "Delta Time : " + to_string(dt) << endl; //devra etre fixe
+    cout << "Nombre de boules : " + to_string(nbBoule) << endl;
+    cout << "Taille boules : " + to_string(bouleSize) << endl; //devra etre fixe
+    cout << "Seed for noise : " + to_string(seed) << endl;
+    cout << "Noise factor : " + to_string(noise) << endl;
+    //cout << "Nombre brasseur : " + to_string(nbBrasseur) << endl; //devra etre fixe
+    cout << "Brasseur speed : " + to_string(brasseurSpeed) << endl; //devra etre fixe
+    //cout << "Brasseur size : " + to_string(brasseurSize) << endl; //devra etre fixe
+    cout << "Square size : " + to_string(WIN_WIDTH) << endl; //devra etre fixe
+    cout << "Nombre frame skip :" + to_string(nbFrameSkip) << endl; //devra etre fixe
+    cout << "Nombre tirage : " + to_string(nbTirage) << endl;
+    cout << "intervalle tirage : " + to_string(timebtwTirage) << endl;
 }
